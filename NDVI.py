cf56141f73fb963798ff99d7c37b633f0216e786
Weixing <weixing.zhang@uconn.edu>
2018-11-01 20:41:59 -0400

    Improved some codes
    improved the script and uploaded a sample dataset

--- NDVI.py
+++ NDVI.py
@@ -1,29 +1,23 @@
-
-'''
+"""
 Acknowlegement:
 @ Script Created on May 3rd 2016
 @ Author: Weixing Zhang
-@ Purpose: Calculate NDVI for large image
+@ Purpose: Calculate NDVI for large images
+@ python 2.76
+@ Required modules: gdal, os.path, numpy, and argparse
 
-@ >>>>>>>>>>>>>> Statement >>>>>>>>>>>>>>
+I thank Roger Veciana i Rovira for sharing his code about raster classification
+Link:http://geoexamples.blogspot.com/2013/06/gdal-performance-raster-classification.html
+I learned and gained a lot of help from Python community. Thank you!
+I share this script mainly because back then, I could not find a easy-to-use Python script to
+calculate NDVI for large RS images. I hope this script will save someone's time! 
 
-First off, I have to thank Roger Veciana i Rovira for sharing his code about raster classification
-           URL: http://geoexamples.blogspot.com/2013/06/gdal-performance-raster-classification.html
-Secondly, I learned a lot and got tons of help from fantastic python community. Thank them as well.
-
-I share this script because I am a simple and lazy guy and I can't find a easy-to-use Python script to
-calculate NDVI for Large! Raster! Data! I was angry, since I had to do it! I hope this script can save
-someone's time! 
-'''
-
-#  Instruction below was implementd based on Windows
-
-
-
-### Step 1: Install gdal module      download from >>>> http://www.lfd.uci.edu/~gohlke/pythonlibs/
-#   (1) Download gdal if you don't have it installed.
-#   (2) After download gdal.whl, open your command prompt, type "cd Downloads", then hit "Enter"
-#   (3) type "pip install xxxx.whl", then "Enter"
+Prerequirement
+Install gdal module
+(1) Download gdal if you don't have it installed on your Machine (http://www.lfd.uci.edu/~gohlke/pythonlibs/)
+(2) After downloaded gdal.whl, open your command prompt, type "cd /Downloads directory"
+(3) type "pip install xxxx.whl"
+"""
 
 # import required modules
 import os.path
@@ -31,126 +25,96 @@ import gdal
 from gdalconst import *
 import numpy as np
 
+"""
+COMMANDLINE: 
+python NDVI.py -block 500 -redband 1 -NIRband 4 -i C:\Users\Wei\Documents\GitHub\NDVI-for-Large-Raster-Python\sample.tif -o C:\Users\Wei\Documents\GitHub\NDVI-for-Large-Raster-Python\sample_NDVI.tif
+"""
 
+if __name__ == '__main__':
+    # ___________________________Arguments___________________________
+    import argparse
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Calculate NDVI for large images.')
 
-### Step 2: set file path
-#   (1) Specify paths of the input and output raster 
-#   (2) Specify names of the input and output raster 
-#   (3) You have to know 
+    parser.add_argument('-block', required=True,
+                        help='Size of processing block by pixel')
 
-# set folder path
-inroot = r'C:\Users\xing\Google Drive\00_Reaserch\05_LanduseClassification\02_Data\NYC\StudyArea_01\areial images'
-outroot = r'E:\New folder'
+    parser.add_argument('-redband', required=True,
+                        help='Order of red band in input raster dataset')
 
-# set input and output tiff name
-inputRaster_name = 'studyarea_aerial_mosaic_cliped_18N_1m.tif'
-outputRaster_name = 'studyarea_aerial_mosaic_cliped_18N_1m_NDVI.tif'
+    parser.add_argument('-NIRband', required=True,
+                        help='Order of NIR band in input raster dataset')
 
-# specify order of red band in input raster
-redband_num = 1
+    parser.add_argument('-i', required=True,
+                        help='Directory of input raster dataset')
 
-# specify order of near-infrared band in input raster
-NIRband_num = 4
+    parser.add_argument('-o', required=True,
+                        help='Directory of output raster dataset')
+    args = parser.parse_args()
 
+    # other parameters
+    block_size = int(args.block)     # Size of processing block by pixel
+    redband_num = int(args.redband)  # Order of red band in input raster
+    NIRband_num = int(args.NIRband)  # Order of NIR band in input raster
 
+    # direcotries of input and output rasters 
+    inputRaster_path = args.i        # Direcotry of input
+    outputRaster_path = args.o       # Direcotry of output
 
-### Step 3: Run this script, then you are done 
+    """
+    Or you can make your own parameters and input and output directory
+    For example:
+    block_size = 500
+    redband_num = 1
+    NIRband_num = 4
 
+    inputRaster_path = r"G:\sample.tif"
+    outputRaster_path = r"G:\sample_ndvi.tif"
+    """
 
-'''
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # _______________________________________________________________
+    # read input rows, cols, and bands of raster
+    ds = gdal.Open(inputRaster_path, GA_ReadOnly)
+    ysize = ds.RasterYSize
+    xsize = ds.RasterXSize
+    nbands = ds.RasterCount
 
-From below on, only for people who would be interested more about python using in raster processing
-For others, just ignore them
+    # read as raster 
+    redband_raster = ds.GetRasterBand(redband_num)
+    nirband_raster = ds.GetRasterBand(NIRband_num)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-'''
+    # output parameters
+    format = "GTiff"  
+    driver = gdal.GetDriverByName(format)
+    dst_ds = driver.Create(outputRaster_path, xsize, ysize, 1, gdal.GDT_Float32)  
+    dst_ds.SetGeoTransform(ds.GetGeoTransform())  
+    dst_ds.SetProjection(ds.GetProjection())  
 
+    # process image in a manners of block by block
+    for i in xrange(0, ysize, block_size):
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# input raster
-inputRaster_path = os.path.join(inroot, inputRaster_name)
-outputRaster_path = os.path.join(outroot, outputRaster_name)
-
-# read input rows, cols, and bands of raster
-ds = gdal.Open(inputRaster_path, GA_ReadOnly)
-nrows = ds.RasterYSize
-ncols = ds.RasterXSize
-nbands = ds.RasterCount
-
-# read as raster 
-redband_raster = ds.GetRasterBand(redband_num)
-nirband_raster = ds.GetRasterBand(NIRband_num)
-
-# set up block size as 500 pixels, you can actually set an larger number if your ram is big enough
-block_size = 500
-
-# set up output parameters
-format = "GTiff"  
-driver = gdal.GetDriverByName(format)  
-dst_ds = driver.Create(outputRaster_path, ncols, nrows, 1, gdal.GDT_Float32)  
-dst_ds.SetGeoTransform(ds.GetGeoTransform())  
-dst_ds.SetProjection(ds.GetProjection())  
-
-# segment rows and cols
-ysize = nrows
-xsize = ncols
-
-# read row
-for i in xrange(0, ysize, block_size):
-
-    # don't want moving window to be larger than row size of input raster
-    if i + block_size < ysize:  
-        rows = block_size  
-    else:  
-        rows = ysize - i
-        
-    # read col      
-    for j in xrange(0, xsize, block_size):
-
-        # don't want moving window to be larger than col size of input raster
-        if j + block_size < xsize:  
-            cols = block_size  
-        else:  
-            cols = xsize - j 
-        
-        # get block out of the whole raster
-        red_array = redband_raster.ReadAsArray(j, i, cols, rows) 
-        nir_array = nirband_raster.ReadAsArray(j, i, cols, rows)
-
-        # avoid zero situation even it won't happen commonly
-        nir_array = nir_array+0.01
-
-        # calculate NDVI
-        ndvi = (nir_array - red_array) / (nir_array + red_array) 
-
-        # write ndvi array to tiff file
-        dst_ds.GetRasterBand(1).WriteArray(ndvi, j, i) 
+        # prevent moving window from being larger than row size of input raster
+        rows = block_size if i + block_size < ysize else ysize - i
             
-# ends program
-dst_ds = None  
-print 'Program ends'
+        # read col      
+        for j in xrange(0, xsize, block_size):
+
+            # prevent moving window from being larger than col size of input raster
+            cols = block_size if j + block_size < xsize else xsize - j
+
+            # extract block out of the whole raster
+            red_array = redband_raster.ReadAsArray(j, i, cols, rows) 
+            nir_array = nirband_raster.ReadAsArray(j, i, cols, rows)
+
+            # avoid zero situation
+            nir_array = nir_array+0.00001
+
+            # calculate NDVI
+            ndvi_array = (nir_array - red_array) / (nir_array + red_array) 
+
+            # write ndvi array into output .tiff file
+            dst_ds.GetRasterBand(1).WriteArray(ndvi_array, j, i) 
+                
+    # program ends
+    dst_ds = None
--- README.md
+++ README.md
@@ -1,15 +1,26 @@
-# Calculate NDVI for Large Raster Dataset by Python
+# Calculate NDVI for large remoted sensing images by Python
 
-Step 1: Install gdal module      download from >>>> http://www.lfd.uci.edu/~gohlke/pythonlibs/
-(1) Download gdal if you don't have it installed.
-(2) After download gdal.whl, open your command prompt, type "cd Downloads", then hit "Enter"
-(3) type "pip install xxxx.whl", then "Enter"
+Acknowlegement:
+@ Script Created on May 3rd 2016
+@ Author: Weixing Zhang
+@ Purpose: Calculate NDVI for large images
+@ python 2.76
+@ Required modules: gdal, os.path, numpy, and argparse
 
+I thank Roger Veciana i Rovira for sharing his code about raster classification
+Link:http://geoexamples.blogspot.com/2013/06/gdal-performance-raster-classification.html
+I learned and gained a lot of help from Python community. Thank you!
+I share this script mainly because back then, I could not find a easy-to-use Python script to
+calculate NDVI for large RS images. I hope this script will save someone's time! 
 
-Step 2: set file path
-(1) Specify paths of the input and output raster 
-(2) Specify names of the input and output raster 
-(3) You have to know 
+(1) You can run it directly from the command line as such:
 
+python NDVI.py -block 500 -redband 1 -NIRband 4 -i G:\input.tif -o G:\output.tif
 
-Step 3: Run this script, then you are done 
+(2) You can customize your own parameters and input and output directory in the script as such:
+ 
+block_size = 500                     # Size of processing block by pixel
+redband_num = 1                      # Order of red band in input raster
+NIRband_num = 4                      # Order of NIR band in input raster
+inputRaster_path = r"G:\input.tif"   # Direcotry of input
+outputRaster_path = r"G:\output.tif" # Direcotry of output
